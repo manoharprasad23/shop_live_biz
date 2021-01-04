08ad55b4fb50df135439b1b1a0440e2915953400
manoharprasad23 <manoharprasad23@gmail.com>
2021-01-04 13:45:16 +0530

    initial commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="shop-live">shop live</div>
+            <img className="logo" src="https://www.turtlemint.com/wp-content/themes/turtlemint/assets/images/logo.svg" alt="logo"/>
+            <div className="right-nav">
+                <span className="order-status">Order Status</span>
+                <span className="sign-in">Sign In</span>
+                <span className="sign-up">Sign Up</span>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div id="fb-root"></div>
+  <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
+
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
