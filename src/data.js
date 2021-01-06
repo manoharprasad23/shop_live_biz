8d82b9fe852ba58929576721f31585d573e30ef4
manoharprasad23 <manoharprasad23@gmail.com>
2021-01-06 18:39:34 +0530

    initial seller page

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const items = {
+    sellerItems:[
+        {
+            url: "https://images-na.ssl-images-amazon.com/images/I/61HSxuSHiKL._SL1100_.jpg",
+            name: "pTron Bassbuds",
+            description: "Headphones with Hi-Fi Deep Bass",
+            price: "$20",
+            id: 1
+        },
+        {
+            url: "https://images-na.ssl-images-amazon.com/images/I/61aBH%2BcXL2L._SL1100_.jpg",
+            name: "PTron Bullet",
+            description: "Fast Charging",
+            price: "$30",
+            id: 2
+        },
+        {
+            url: "https://images-na.ssl-images-amazon.com/images/I/51-c-jZ2PbL._SL1000_.jpg",
+            name: "HUL Pureit RO",
+            description: "7L Water Purifier",
+            price: "$40",
+            id: 3
+        }
+    ]
+}
